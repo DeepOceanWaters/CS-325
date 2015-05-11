321503a3283863f16934a3fc7968ddb16f7ac78a
le90 <albertle5@gmail.com>
2015-05-10 17:04:03 -0700

    plotting code

--- projects/project2/project2.py
+++ projects/project2/project2.py
@@ -1,6 +1,10 @@
 import ast
 import sys
 import copy
+import timeit
+
+results= [[],[],[]]
+results1= [[],[],[]] 
 
 def main():
     args = sys.argv
@@ -10,6 +14,9 @@ def main():
     results_slow = []
     results_greedy = []
     results_dp = []
+    plotting4()
+    plotting5()
+    plotting6()
     with open(args[1] + ".txt", "r") as f:
         coins = f.readline()
         while coins != '':
@@ -18,11 +25,11 @@ def main():
             coins = f.readline()
     with open(args[1] + "change.txt", "w") as f:
         for test in tests:
-            results_slow += changeslow(test[0], test[1])
+            #results_slow += changeslow(test[0], test[1])
             results_greedy += changegreedy(test[0], test[1])
             results_dp += changedp(test[0], test[1])
-        f.write("slow: " + repr(results_slow))
-        f.write("\n")
+        #f.write("slow: " + repr(results_slow))
+        #f.write("\n")
         f.write("greedy: " + repr(results_greedy))
         f.write("\n")
         f.write("dp: " + repr(results_dp))
@@ -83,6 +90,84 @@ def changedp(V, A):
         j -= V[i]
     return C, T[A]
 
+def part4():
+    V = [1,5,10,25,50]
+    A = []
+    for i in range(1000010,1000205,5):
+        A.append(i)
+    return V,A
+        
+def part5():
+    V1 = [1,2,6,12,24,48,60]
+    V2 = [1,6,13,37,150]
+    A = []
+    bigA = []
+    for i in range(1000000,1000201):
+        A.append(i)
+        bigA.append(i+8000)
+    return V1,V2,A,bigA
+    
+def part6():
+    V = [1]
+    A = []
+    for i in range(2,30,2):
+        V.append(i)
+    
+    for i in range(1000000,1000201):
+        A.append(i)    
+    return V,A
+   
+def plotting4():
+    V,A = part4()
+    setupgreedy = "from __main__ import changegreedy"
+    setupdp = "from __main__ import changedp"
+    for i in A:
+        testgreedy = "changegreedy(%s,%s)"%(V,i)
+        testdp = "changedp(%s,%s)"%(V,i)
+        resultgreedy = timeit.timeit(testgreedy, setup=setupgreedy, number=10)
+        resultdp = timeit.timeit(testdp, setup=setupdp, number=10)
+        results[1].append(resultgreedy)
+        results[2].append(resultdp)
+    p = open("plotting4.txt",'a')
+    for i in range(len(A)):
+        p.writelines("%d\t%f\t%f\n"%(A[i],results[1][i],results[2][i]))
+    
+def plotting5():
+    V1,V2,A, bigA = part5()
+    setupgreedy = "from __main__ import changegreedy"
+    setupdp = "from __main__ import changedp"
+    for i in bigA:
+        testgreedy1 = "changegreedy(%s,%s)"%(V1,i)
+        testgreedy2 = "changegreedy(%s,%s)"%(V2,i)
+        testdp1 = "changedp(%s,%s)"%(V1,i)
+        testdp2 = "changedp(%s,%s)"%(V2,i)
+        resultgreedy1 = timeit.timeit(testgreedy1, setup=setupgreedy, number=10)
+        resultgreedy2 = timeit.timeit(testgreedy2, setup=setupgreedy, number=10)
+        resultdp1 = timeit.timeit(testdp1, setup=setupdp, number=10)
+        resultdp2 = timeit.timeit(testdp2, setup=setupdp, number=10)
+        results[1].append(resultgreedy1)
+        results[2].append(resultdp1)
+        results1[1].append(resultgreedy2)
+        results1[2].append(resultdp2)
+    p = open("plotting5.txt",'a')
+    for i in range(len(A)):
+        p.writelines("%d\t%f\t%f\t%f\n"%(A[i],results[1][i],results1[1][i],results[2][i],results1[2][i]))
+
+def plotting6():
+    V,A = part6()
+    setupgreedy = "from __main__ import changegreedy"
+    setupdp = "from __main__ import changedp"
+    for i in A:
+        testgreedy = "changegreedy(%s,%s)"%(V,i)
+        testdp = "changedp(%s,%s)"%(V,i)
+        resultgreedy = timeit.timeit(testgreedy, setup=setupgreedy, number=10)
+        resultdp = timeit.timeit(testdp, setup=setupdp, number=10)
+        results[1].append(resultgreedy)
+        results[2].append(resultdp)
+    p = open("plotting6.txt",'a')
+    for i in range(len(A)):
+        p.writelines("%d\t%f\t%f\n"%(A[i],results[1][i],results[2][i]))
+      
 
 # Call main function
 if __name__ == '__main__':
