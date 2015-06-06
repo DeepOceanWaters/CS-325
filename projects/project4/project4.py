73c95dd7c74f652ed61e3fc62a6f298320efd5df
Charles Jenkins <charlesmjenkins@users.noreply.github.com>
2015-06-05 21:29:52 -0700

    Got greedy algo working for simple three node case. Debug statements still in code but commented out.

--- projects/project4/project4.py
+++ projects/project4/project4.py
@@ -38,7 +38,7 @@ def main():
         for line in f:
             cities.append(line.split()[1:])
         
-    print cities
+    # print cities
     
     degrees = [0 for i in range(len(cities))]
     
@@ -48,7 +48,7 @@ def main():
         y = int(y)
         coords.append((x, y))
         
-    print coords
+    # print coords
 
     # Initialize distance table
     distanceTable = [[None for i in range(len(cities))] for j in range(len(cities))]
@@ -62,14 +62,16 @@ def main():
                 distanceTable[i][j] = distance
                 distList.append((distance, i, j))
                 j += 1
-    print distanceTable
-    print distList
+    # print distanceTable
+    # print distList
     
-    print sorted(distList)
+    # print sorted(distList)
     
     # # Create file, execute algorithms, and write results to file
     # with open(args[1] + ".tour", "w") as f:
         # f.write(TSP(sorted(distList)))
+    
+    TSP(sorted(distList), degrees)
        
 # ---------------------------------------
 # Name: TSP
@@ -78,26 +80,91 @@ def main():
 # TSP by using the greedy method.
 #
 # Receives: 
-# List of cities and coordinates
+# Sorted list of tuples (distance, x, y)
 #
 # Returns:
 # Cost, Route
 # ---------------------------------------
-def TSP(D):
+def TSP(D, degrees):
     cost = 0
     route = []
     
-    # Add lowest cost edge to route
+    # print D
+    
+    # route.append(D[0][1])
+    # del D[0]
+    # degrees[0] += 1
+    c1, x1, y1 = D[0]
+
+    # print "inside TSP"
+    # print route
+    # print degrees
+    
+    # Add lowest cost edge to route.
     # Select each subsequent edge such
     # that it keeps all vertex degrees < 3
     # and so no cycles are formed until
     # the number of selected edges equals
     # the number of vertices.
+    while len(route) <= len(degrees):
+        # print "inside while"
+        for i in D:
+            # print i
+            # print "inside for"
+            x2 = i[1]
+            y2 = i[2]
+            # print "D = " + str(D)
+            # print "x2: " + str(x2)
+            # print "y2: " + str(y2)
+            if degrees[i[1]] < 2 and y1 == x2 and degrees[y2] == 0 and x2 != y2:
+                # print "inside if"
+                # print i
+                # print "appending "
+                # print i[1]
+                route.append(i[1])
+                cost += i[0]
+                degrees[x1] += 1
+                x1 = x2
+                y1 = y2
+                del D[D.index(i)]
+                # print "route: "
+                # print route
+                # if len(route) == len(degrees):
+                    # break
+        
+        if len(route) == len(degrees):
+            break
+    
+    # Append final node to complete circuit
     for i in D:
-        if degrees[i] < 3:
-            route.append(D[i][0])
+        # print "inside for"
+        x2 = i[1]
+        y2 = i[2]
+        # print "D = " + str(D)
         
+        # print "x1: " + str(x1)
+        # print "y1: " + str(y1)
         
+        # print "x2: " + str(x2)
+        # print "y2: " + str(y2)
+        
+        if y2 == route[0]:
+            # print "inside if2"
+            # print i
+            # print "appending "
+            # print i[1]
+            route.append(i[1])
+            cost += i[0]
+            degrees[x1] += 1
+            x1 = x2
+            y1 = y2
+            del D[D.index(i)]
+            # print "route: "
+            # print route
+            break
+    
+    print cost
+    print route
     
     return cost, route
     
